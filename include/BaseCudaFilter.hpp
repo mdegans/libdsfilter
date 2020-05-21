8b07fdd9deae0c2531c1215bdb7c1cdd995c732e
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-21 04:23:43 +0000

    BaseFilter with no cuda stream * for deepstream metadata plugins

--- include/BaseCudaFilter.hpp
+++ include/BaseCudaFilter.hpp
@@ -7,10 +7,12 @@
 
 #include <cuda_runtime.h>
 
-#include "gstnvdsmeta.h"
-#include "nvbufsurface.h"
+#include "BaseFilter.hpp"
 
-class BaseCudaFilter {
+/**
+ * Base class for filters needing a CUDA stream.
+ */
+class BaseCudaFilter: public BaseFilter {
  protected:
   // cuda stream to use for a filter
   cudaStream_t stream;
@@ -18,31 +20,6 @@ class BaseCudaFilter {
  public:
   BaseCudaFilter();
   virtual ~BaseCudaFilter();
-
-  /**
-   * Called on every NVMM batched buffer.
-   *
-   * Should be connected to a buffer callback or used in a filter plugin.
-   *
-   * return a GstFlowReturn (success, failure, etc.)
-   */
-  virtual GstFlowReturn on_buffer(GstBuffer* buf);
-
-  /**
-   * Called on every frame by on_buffer
-   *
-   * return true on success, false on failure
-   */
-  virtual bool on_frame(NvBufSurface* surf, NvDsFrameMeta* frame_meta);
-
-  /**
-   * Called on every object meta by on_frame
-   *
-   * return true on success, false on failure
-   */
-  virtual bool on_object(NvDsFrameMeta* f_meta,
-                         NvDsObjectMeta* o_meta,
-                         NvBufSurfaceParams* frame);
 };
 
 #endif  // BASE_CUDA_FILTER_HPP_
