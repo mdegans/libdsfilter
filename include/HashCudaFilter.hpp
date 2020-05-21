dbdde142de0ed5a1c086d17bfde7f1a4e5cfc0b7
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-05-21 04:27:49 +0000

    add hashcudafilter

---
+++ include/HashCudaFilter.hpp
@@ -0,0 +1,28 @@
+#ifndef HASH_CUDA_FILTER_HPP_
+#define HASH_CUDA_FILTER_HPP_
+
+#pragma once
+
+#include "BaseCudaFilter.hpp"
+
+typedef char Hash[64];
+typedef char SmHash[16];
+
+/**
+ * HashCudaFilter hashes.
+ */
+class HashCudaFilter : public BaseCudaFilter {
+private:
+  Hash& hash;
+  SmHash& sm_hash;
+public:
+  /**
+   * on_frame calculates a simple image hash for each frame
+   */
+  bool on_frame(NvBufSurface* surf, NvDsFrameMeta* frame_meta);
+
+  const Hash& getHash() const { return hash; };
+  const SmHash& getSmHash() const { return sm_hash; }
+};
+
+#endif  // TEST_CUDA_FILTER_HPP_
