b6b2daab52fe15169a61ba8b7d77e72a81097bd8
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-06-17 14:48:11 -0700

    tegra support (#3)

--- docker_build.sh
+++ docker_build.sh
@@ -10,19 +10,30 @@ TAG_SUFFIX=$(git rev-parse --abbrev-ref HEAD)
 if [[ $TAG_SUFFIX == "master" ]]; then
     TAG_SUFFIX="latest"
 fi
-readonly DOCKERFILE_BASENAME="${TAG_SUFFIX}.Dockerfile"
+readonly DOCKERFILE_BASENAME="latest.Dockerfile"
 
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 readonly DOCKERFILE="$THIS_DIR/$DOCKERFILE_BASENAME"
 readonly VERSION=$(head -n 1 $THIS_DIR/VERSION)
 readonly TAG_BASE="$AUTHOR/$PROJ_NAME"
-readonly TAG_FULL="$TAG_BASE:$VERSION"
+TAG_FULL="$TAG_BASE:$VERSION"
+
+# TODO(mdegans): using deepstream-$(arch) may be cleaner
+if [[ "$(arch)" == "aarch64" ]]; then
+    readonly DISTANCEPROTO_TAG="deepstream-tegra"
+    readonly TAG_SUFFIX="${TAG_SUFFIX}-tegra"
+    readonly TAG_FULL="${TAG_FULL}-tegra"
+else
+    readonly DISTANCEPROTO_TAG="deepstream-x86"
+    readonly TAG_SUFFIX="${TAG_SUFFIX}-x86"
+    readonly TAG_FULL="${TAG_FULL}-x86"
+fi
 
 echo "Building $TAG_FULL from $DOCKERFILE"
 
-docker build --rm -f $DOCKERFILE \
-    --build-arg DISTANCEPROTO_TAG="$VERSION-ds" \
+docker build --pull --rm -f $DOCKERFILE \
+    --build-arg DISTANCEPROTO_TAG="$DISTANCEPROTO_TAG" \
     -t $TAG_FULL \
     $THIS_DIR $@
 docker tag "$TAG_FULL" "$TAG_BASE:$TAG_SUFFIX"
